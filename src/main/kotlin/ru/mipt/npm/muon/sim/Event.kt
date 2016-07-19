42b292f46a7dea8629384fb354f7cda9252ba98b
Alexander Nozik <altavir@users.noreply.github.com>
2016-07-19 20:23:40 +0300

    Reading Almaz file and comparing experiment with simulation

--- src/main/kotlin/ru/mipt/npm/muon/sim/Event.kt
+++ src/main/kotlin/ru/mipt/npm/muon/sim/Event.kt
@@ -6,28 +6,28 @@ import java.util.*
  * Created by darksnake on 11-May-16.
  */
 
-fun buildEventByTrack(track: Track, hitResolver: (Track) -> List<Pixel> = defaultHitResolver): Event {
+fun buildEventByTrack(track: Track, hitResolver: (Track) -> Collection<Pixel> = defaultHitResolver): Event {
     return Event(track, hitResolver.invoke(track))
 }
 
 
-
-data class Event(val track: Track, val hits: List<Pixel>){
+data class Event(val track: Track, val hits: Collection<Pixel>) {
     /**
-     * Ge unique identity for given set of hits. One identity could correspond to different tracks
+     * The unique identity for given set of hits. One identity could correspond to different tracks
      */
-    fun getIdentity(): String{
-        return hits.joinToString(separator = ", ", prefix = "[", postfix = "]",transform = {pixel->pixel.name});
+    fun getIdentity(): String {
+        return hits.sortedBy { it -> it.name }
+                .joinToString(separator = ", ", prefix = "[", postfix = "]", transform = { pixel -> pixel.name });
     }
 
 }
 
-val defaultHitResolver: (Track) -> List<Pixel> = { track: Track ->
+val defaultHitResolver: (Track) -> Collection<Pixel> = { track: Track ->
     val hits = HashSet<Pixel>();
     for (p in pixels.values) {
         if (p.isHit(track)) {
             hits.add(p);
         }
     }
-    hits.sortedBy { it -> it.name }
+    hits
 }
