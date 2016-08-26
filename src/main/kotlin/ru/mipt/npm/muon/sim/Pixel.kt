b7770a77e3125897290187f7ccffb9fe39b43272
Alexander Nozik <altavir@users.noreply.github.com>
2016-08-26 11:14:47 +0300

    Track kength calculation.

--- src/main/kotlin/ru/mipt/npm/muon/sim/Pixel.kt
+++ src/main/kotlin/ru/mipt/npm/muon/sim/Pixel.kt
@@ -1,5 +1,6 @@
 package ru.mipt.npm.muon.sim
 
+import org.apache.commons.math3.geometry.euclidean.threed.Plane
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D
 
 /**
@@ -8,22 +9,27 @@ import org.apache.commons.math3.geometry.euclidean.threed.Vector3D
  */
 class Pixel(val name: String, val center: Vector3D, var efficiency: Double = 1.0,
             val xSize: Double = PIXEL_XY_SIZE, val ySize: Double = PIXEL_XY_SIZE, val zSize: Double = PIXEL_Z_SIZE) {
-    val layer: Layer = findLayer(center.z);
-    private val upLayer = Layer("${name}_up", center.z + zSize / 2.0);
-    private val bottomLayer = Layer("${name}_bottom", center.z - zSize / 2.0);
+    //    val layer: Layer = findLayer(center.z);
+    private val upLayer = findLayer(center.z + zSize / 2.0)//Layer("${name}_up", center.z + zSize / 2.0);
+    private val bottomLayer = findLayer(center.z - zSize / 2.0)//Layer("${name}_bottom", center.z - zSize / 2.0);
+
+    val sideLayers: Array<Plane> = arrayOf(Plane(center.add(Vector3D(PIXEL_XY_SIZE / 2, 0.0, 0.0)), Vector3D(1.0, 0.0, 0.0), GEOMETRY_TOLERANCE),
+            Plane(center.add(Vector3D(-PIXEL_XY_SIZE / 2, 0.0, 0.0)), Vector3D(-1.0, 0.0, 0.0), GEOMETRY_TOLERANCE),
+            Plane(center.add(Vector3D(0.0, PIXEL_XY_SIZE / 2, 0.0)), Vector3D(0.0, 1.0, 0.0), GEOMETRY_TOLERANCE),
+            Plane(center.add(Vector3D(0.0, -PIXEL_XY_SIZE / 2, 0.0)), Vector3D(0.0, -1.0, 0.0), GEOMETRY_TOLERANCE));
 
     //TODO add efficiency
-    fun containsPoint(x: Double, y: Double, z: Double): Boolean {
-        return x <= this.center.x + this.xSize / 2.0 && x >= this.center.x - this.xSize / 2.0 &&
-                y <= this.center.y + this.ySize / 2.0 && y >= this.center.y - this.ySize / 2.0 &&
-                z <= this.center.z + this.zSize / 2.0 && z >= this.center.z - this.zSize / 2.0;
+    fun containsPoint(x: Double, y: Double, z: Double, tolerance :Double = GEOMETRY_TOLERANCE): Boolean {
+        return x <= this.center.x + this.xSize / 2.0 + tolerance && x >= this.center.x - this.xSize / 2.0 - tolerance &&
+                y <= this.center.y + this.ySize / 2.0 + tolerance && y >= this.center.y - this.ySize / 2.0 - tolerance &&
+                z <= this.center.z + this.zSize / 2.0 + tolerance && z >= this.center.z - this.zSize / 2.0 - tolerance;
     }
 
     /**
      * Check if pixel contains point
      */
-    fun containsPoint(point: Vector3D): Boolean {
-        return containsPoint(point.x, point.y, point.z);
+    fun containsPoint(point: Vector3D, tolerance :Double = GEOMETRY_TOLERANCE): Boolean {
+        return containsPoint(point.x, point.y, point.z, tolerance);
     }
 
     /**
@@ -32,9 +38,39 @@ class Pixel(val name: String, val center: Vector3D, var efficiency: Double = 1.0
      */
     fun isHit(track: Track): Boolean {
         //check central plane as well as upper and bottom planes of the layer
-        val contains = containsPoint(layer.intersect(track)) ||
-                containsPoint(upLayer.intersect(track)) ||
-                containsPoint(bottomLayer.intersect(track));
-        return contains && (efficiency == 1.0 || rnd.nextDouble() < efficiency);
+        val upperIntersection = upLayer.intersect(track);
+        val bottomIntersection = bottomLayer.intersect(track);
+        val upperHit = containsPoint(upperIntersection);
+        val bottomHit = containsPoint(bottomIntersection);
+
+
+        if (!bottomHit && !upperHit) {
+            return false;
+        } else if (upperHit && bottomHit) {
+            return eff();
+        } else {
+            val verticalHitPoint = when (upperHit) {
+                true -> upperIntersection
+                false -> bottomIntersection
+            }
+            val horizontalHitPoint = getHorizontalHitPoint(track);
+            val length = verticalHitPoint.distance(horizontalHitPoint);
+            return length >= MINIMAL_TRACK_LENGTH;
+        }
+    }
+
+    fun getHorizontalHitPoint(track: Track): Vector3D {
+        for (p in sideLayers) {
+            val intersection = p.intersection(track.line);
+            //FIXME there is a problem with geometric tolerances here
+            if (intersection != null && containsPoint(intersection, 100* GEOMETRY_TOLERANCE)) {
+                return intersection;
+            }
+        }
+        error("No side intersection");
+    }
+
+    private fun eff(): Boolean {
+        return efficiency == 1.0 || rnd.nextDouble() < efficiency;
     }
 }
